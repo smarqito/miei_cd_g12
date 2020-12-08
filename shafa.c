216570cd53d2c825d1619d9bb1343d33c3dcc93a
Marco António Sousa <smarqito@gmail.com>
2020-12-08 21:15:01 +0000

    main.c review ; now all option available

--- shafa.c
+++ shafa.c
@@ -7,27 +7,82 @@
  * ./shafa.exe file_name -m f/t/d
  */
 
-int main (int argc, char *argv[]) {
-    if (argc == 4 && strcmp(argv[2],"-m") == 0 ) {
-        printf("File to read: \"%s\"\n",argv[1]);
-        if (strcmp(argv[3],"f") == 0 ) {
-            // call module f
-            printf("I'm on module: \"-m f\"\n");
-            // must output .rle file
-            // @rle
-        } else if (strcmp(argv[3],"t") == 0) {
-            // call module t
-            printf("I'm on module: \"-m t\"\n");  
-            // must output .cod file
-            // @cod
-        } else if (strcmp(argv[3],"d") == 0 ) {
-            // call module d
-            printf("I'm on module: \"-m d\"\n");
-            // must output .shaf file
-            // @shaf
-        } else {
-            // wtf is module c ???
+int main(int argc, char *argv[])
+{
+    if (argc >= 4 && argc <= 8)
+    {
+        int forceRLE = 0;
+        char byteDimension = 'K';
+        char *module = NULL;
+        int moduleType;
+        char *fileToOpen = argv[1];
+        int optimizeSF_opt = 0;
+
+        for (int i = 2; i < argc; i = i + 2)
+        {
+
+            if (strcmp(argv[i], "-m") == 0)
+            {
+                if (i + 1 < argc)
+                {
+                    module = argv[i];            // get module -m
+                    moduleType = argv[i + 1][0]; // get module type [d, t, f]
+                }
+            }
+            else if (strcmp(argv[i], "-c") == 0)
+            {
+                if (i + 1 < argc && argv[i + 1][0] == 'r')
+                {
+                    forceRLE = 1; //force RLE compression
+                }
+            }
+            else if (strcmp(argv[i], "-b") == 0)
+            {
+                if (i + 1 < argc && (argv[i + 1][0] == 'K' || argv[i + 1][0] == 'm' || argv[i + 1][0] == 'M'))
+                {
+                    byteDimension = argv[i + 1][0]; // [K, m, M]
+                }
+            }
+            else if (strcmp(argv[i], "-d") == 0)
+            {
+                if (i + 1 < argc)
+                {
+                    optimizeSF_opt = (argv[i + 1][0] == 's') ? 1 : (argv[i + 1][0] == 'r') ? 2 : 0;
+                }
+            }
         }
-        return 0;
+
+        if (module)
+        {
+            printf("File to read: \"%s\"\n", fileToOpen);
+            switch (moduleType)
+            {
+            case 'd':
+                // call module d
+                printf("I'm on module: \"-m d\"\n");
+                printf("Options: %c\n", optimizeSF_opt == 1 ? 's' : optimizeSF_opt == 2 ? 'r' : '-');
+                // must output .shaf file
+                // @shaf
+                break;
+            case 'f':
+                // call module f
+                // must output .rle file
+                // @rle
+                printf("I'm on module: \"-m f\"\n");
+                printf("ForceRLE?: %s\n", forceRLE ? "Sim" : "Não");
+                printf("Tamanho: %cbytes\n", byteDimension);
+                break;
+            case 't':
+                // call module t
+                printf("I'm on module: \"-m t\"\n");
+                // must output .cod file
+                // @cod
+                break;
+            default:
+                print("Reveja o comando inserido.\n");
+                break;
+            }
+        }
     }
+    return 0;
 }
