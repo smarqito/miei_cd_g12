d566f32bf7bf189101e55d89dbc58284a1a09cc1
Marco António Sousa <smarqito@gmail.com>
2021-01-04 01:17:06 +0000

    moduyle d update

--- module_d/module_d.c
+++ module_d/module_d.c
@@ -1,62 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "module_d.h"
-#include "module_d_bit.h"
-#include "../module_c/headers/SFCodes.h"
+#include "../module_t/module_t.h"
 
-int readCodFile(SFCodes **blocosSF, char *fileName)
+int decompressRLE(char *RLEFile)
 {
 
-	int nBlocos, symbol = 0, blocoAtual = 0;
-	FILE *fp = fopen(fileName, "r");
-	char codeSF[9], c;
-	int lenCodeSF = 0;
+	FILE *outputFileName = fopen(OG, "w");
+	int nBlocos2, nBlocosInit2, size2;
+	FILE *fpRLE = fopen(RLEFile, "r");
+	fseek(fpRLE, 0, SEEK_SET);
+	for (int q = 0; q < strlen(RLEFile); q++)
+	{
+		if (strcmp(RLEFile[q], '{0}'))
+		{
+			int aux = 0;
+			aux = RLEFile[q + 2];
+			while (aux > 0)
+			{
+				fprintf(outputFileName, "%c", RLEFile[q + 1]);
+				aux--;
+			}
+			q = q + 2;
+		}
+		else
+			fprintf(outputFileName, "%c", RLEFile[q]);
+	}
+	fclose(outputFileName);
+	return 0;
+}
 
-	if (!fp)
+int decompress(char *shafaFile, char *codFile)
+{
+	//verificar o n� de blocos(nBlocos)
+	if (!codFile || !shafaFile)
 		return 0;
 
-	codeSF[0] = '\0';
+	int i;
+	int symbolFreq;
+	char t;
+	int nBlocos, nBlocosInit, size;
+	int simbolosNaoNulos;
+	char tc;
+	//verificar N ou R
+	FILE *fpcod = fopen(codFile, "r");
+	fseek(fpcod, 1, SEEK_SET);
+	fscanf(fpcod, "%c", &tc);
+	if ((tc != 'N') || (tc != 'R'))
+		return 0;
 
-	fseek(fp, 3, SEEK_SET);
-	fscanf(fp, "%d", &nBlocos);
+	nBlocos = nBlocosInit = atoi(seekFromFile(fpcod, '@'));
+	if (!nBlocos)
+		return 0;
 
-	*blocosSF = (SFCodes *)malloc(sizeof(SFCodes) * nBlocos);
-	fseek(fp, 1, SEEK_CUR); //consome @ de bytes bloco
+	//criar uma matriz (nBlocos) linhas
+	//cadalinha 255strings
 
-	while (blocoAtual < nBlocos)
+	char *matriz[nBlocos];
+
+	for (int k = 0; k < nBlocos; k++)
+		matriz[k] = malloc(255 * sizeof(char *));
+
+	fseek(fpcod, 1, -1);
+	//ler cod
+	// Criar o nome de output de forma din�mica, com base no nome do ficheiro de origem
+	// armazena na mesma pasta que o ficheiro de origem
+	char *outputFileName = strdup(shafaFile);
+	strcpy(outputFileName + strlen(shafaFile) - 4, "cod");
+
+	printAuthor(nBlocos);
+
+	while (nBlocos > 0)
 	{
+		size = atoi(seekFromFile(fpcod, '@'));
+		for (i = 0; i < 255; i++)
+		{
+			matriz[nBlocos][i] = seekFromFile(fpcod, ';');
+		}
 
-		(*blocosSF)[blocoAtual] = initializeSFCodes();
+		seekFromFile(fp, '@');
 
-		symbol = 0;
+		nBlocos--;
+	}
 
-		fscanf(fp, "%D", $sizeBloco);
-		setBytes((*blocoSF)[blocoAtual], sizeBloco);
+	fclose(fpcod);
 
-		fseek(fp, 1, SEEK_CUR); //consome @ de inicio bloco
-		while (symbol < 256)
-		{ // leitura bloco individual
-			c = fgetc(fp);
+	//ler shafa
+	char ts;
+	int nBlocos1, nBlocosInit1, size1;
+	FILE *fpshafa = fopen(shafaFile, "r");
+	fseek(fpshafa, 1, SEEK_SET);
+	fscanf(fpshafa, "%c", &ts);
+	if ((ts != 'N') || (ts != 'R'))
+		return 0;
+	nBlocos1 = nBlocosInit1 = atoi(seekFromFile(fpshafa, '@'));
+	if (!nBlocos1)
+		return 0;
+	seekFromFile(fpshafa, '@');
+	for (int q = 0; q < strlen(shafaFile); q++)
+	{
+		if (seekFromFile(fpshafa, '@'))
+		{
 
-			if ((c == ';' || c == '@') && lenCodeSF == 0)
+			while (nBlocosInit > 0)
 			{
-				symbol++;
-			}
-			else if (c == ';' || c == '@')
-			{
-				setCodeSF((*blocoSF)[blocoAtual], symbol, codeSF);
-				lenCodeSF = 0;
-				codeSF[0] = '\0';
-				symbol++;
-			}
-			else
-			{
-				codeSF[lenCodeSF++] = c;
-				codeSF[lenCodeSF] = '\0';
+				size = atoi(seekFromFile(fpcod, '@'));
+				for (int i = 0; i < 255; i++)
+				{
+					if (strcmp(matriz[nBlocos][i], shafaFile[q]))
+					{
+						fprintf(outputFileName, "%c", i);
+					}
+				}
+
+				nBlocosInit--;
 			}
 		}
-		blocoAtual++;
 	}
-	fclose(fp);
-	return nBlocos;
+
+	return 0;
 }
--- module_d/module_d.h
+++ module_d/module_d.h
@@ -1,12 +1,6 @@
 #ifndef MODULE_D
 #define MODULE_D
 
-typedef struct sfc{
-    char** codes;
-    int nBytes;
-    int nBite_compressed;
-} *SFCodes;
 
-int readCodFile(SFCodes **blocosSF, char *fileName);
 
 #endif
